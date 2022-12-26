05d91694f4dc8d5f6dda16d2ae96fca1f910f1f4
GP10Dev <104908091+GP10Dev@users.noreply.github.com>
2022-12-26 12:34:30 +0300

    closing xmas

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AgriMech Solutions</title>
+
+    <link href="./static/css/index.css" rel="stylesheet" />
+</head>
+<body>
+    <header>
+        <?php require "./static/css/includes/header.html" ; ?>
+    </header>
+    <!-- main -->
+    <section class="main">
+        <div id="scn1">
+            <div>
+                <h2>Service - Repair - Replace</h2>
+                <button>Call us now</button>
+            </div>
+        </div>
+        <div id="scn2">
+            <h2>Service and Repairs</h2>
+            <table>
+                <tr>
+                    <td>
+                        <img src="" alt="service pic agri Mech"/>
+                    </td>
+                    <td>
+                        <img src="" alt="Replace pic agri Mech"/>
+                    </td>
+                    <td>
+                        <img src="" alt="repair pic agri Mech"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>service</td>
+                    <td>Replace</td>
+                    <td>Repair</td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>We pride ourselves in the quality of our repair work and our passion for farm machinery. Since 2001 we have expertly restored many vintage and cherished tractors. If you have an antique tractor that needs restoring, we can help.</p>
+                    </td>
+                    <td>
+                        <p>We are able to supply new farming equipment from a large list of dealers. We have many items in stock for sale, including the Landini range of tractors, new and used tractors, ATVs and farm machinery. </p>
+                    </td>
+                    <td>
+                        <p>We provide a fast, reliable service to repair and service ALL agricultural machinery at competitive prices.  </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Find out more</td>
+                    <td>Find out more</td>
+                    <td>Find out more</td>
+                </tr>
+            </table>
+        </div>
+        <div>
+            <p>Since 2022, AgriMech Solutions has been supporting the farming community in Banda and surrounding areas, by providing top quality servicing and repairs to tractors and all types of farm vehicles and equipment. Our experienced and skilled team of mechanics have been able to do this either at our customer's premises or at our workshop.</p>
+            <p>We also are main dealers for a wide range of farming equipment, including the amazing range of Landini tractors. So whether you need to Repair, Replace or Restore, give us a call on  +256706083050</p>
+        </div>
+    </section>
+    <!-- end main -->
+    <footer>
+        <?php require "./static/css/includes/footer.html" ; ?>
+    </footer>
+</body>
+</html>
---
+++ services.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AgriMech Solutions</title>
+
+    <link href="./static/css/index.css" rel="stylesheet" />
+</head>
+<body>
+    <header>
+        <section class="header-col" id="head">
+            <div>
+                <h1>AgriMech Solutions</h1>
+            </div>
+        </section>
+        <section class="header-col">
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Service & Products</li>
+                    <li>Contact us</li>
+                    <li>Careers</li>
+                    <li>Latest</li>
+                    <li class="button">Hire us</li>
+                </ul>
+            </nav>
+        </section>
+        <div class="cls"></div>
+    </header>
+    <!-- main -->
+    <section class="main">
+        <div id="scn1">
+            <div>
+                <h2>Service - Repair - Replace</h2>
+            </div>
+        </div>
+        <div id="scn2">
+            <h2>Service and Repairs</h2>
+            <p>AgriMech Solutins provides well trained and experienced mechanics who have varied field expertise to meet your needs.</p>
+            <p>We provide full range of services and maintenance for all tractors, combines, ATV's, agricultural machinery and plant equipment. Parts can be delivered quickly and fitted straight away. Based in <b>Banda, Kampala,</b> we can offer both onsite and workshop repiar and quality workmanship. To book an onsite visit or for your equipment to be serviced or repaired at our premises, call now on +256706083050.</p>
+            <p>we have a wide range of quality parts which can be bought directly from our trade counter. Why not visit us now and see what we have for you?</p>
+            <p>We also offer <b>Engine Diagnostic Testing upto 300 HP.</b></p>
+        </div>
+    </section>
+    <!-- end main -->
+    <footer>
+        <?php require "./includes/footer.html" ; ?>
+    </footer>
+</body>
+</html>
